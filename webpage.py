b7607d12e0902f2fd5cd0f1ab6e76c81b56718d5
christopherh2 <77631390+christopherh2@users.noreply.github.com>
2021-10-18 14:58:58 -0400

    Added server side error handling

--- webpage.py
+++ webpage.py
@@ -1,13 +1,13 @@
 from flask import Flask, request, url_for, redirect, render_template, flash
-import pickle 
+import pickle, os
 import pandas as pd
 import numpy as np
-import os
+from pycaret.regression import *
 
 # Change the template folder as necessary
-app = Flask(__name__, template_folder='C:\\Users\\Chris\\Desktop\\flaskr\\2 pg')
+app = Flask(__name__, template_folder='os.getcwd()')
 
-# home screen
+# home screen renders pg 1 file
 @app.route('/')
 def home():
     return render_template('pg1.html')
@@ -16,11 +16,57 @@ def home():
 # Redirect to pg2 when form is submitted
 @app.route('/pg2', methods=['POST'])
 def submit():
+    # Method will be post on form submit
     if request.method== 'POST':
-        fname = request.form['fname']
-        lname = request.form['lname']
-        title = request.form['title']
+        result=request.form
+        model = result.get('model')
         
-    
-    return render_template('pg2.html', title=title, fname=fname, lname=lname)
+        # Server side form validation
+        try:
+            # Cast data as int or float to ensure data is in appropriate form
+            year= int(result.get('year'))
+            mileage= int(result.get('mileage'))
+            engineSize= float(result.get('engineSize'))
+            mpg = float(request.form.get('mpg'))
+
+        except:
+            # If a data type is not valid return error
+            error= "Enter a valid number!"
+            return render_template('pg1.html',error=error)
+        
+        else:
+            # Check if form is filled out entirely
+            if not model or not year or not mileage or not mpg or not engineSize:
+                error="Error! Fill all the fields out!"
+                return render_template('pg1.html',error=error)
+            
+            # Check if numbers are positive
+            if mileage <0:
+                error ="Enter a valid mileage!"
+                return render_template('pg1.html',error=error)
+            
+            if mpg <=0:
+                error ="Enter a valid mpg value!"
+                return render_template('pg1.html',error=error)
+                
+            if engineSize <=0:
+                error ="Enter a valid engine size!"
+                return render_template('pg1.html',error=error)    
+                
+                
+            # Load model and set columns
+            model = load_model("model2")
+            cols = ['model','year','mileage','mpg','engineSize']
+            data = [x for x in request.form.values()]
+            final=np.array(data)
+
+            df = pd.DataFrame(data=[final], columns=cols)
+
+            pred= predict_model(model, data=df)
+            prediction = round(pred.Label[0],2)
+        
+        return render_template('pg2.html', data=data, cols=cols, prediction=prediction)
+
+if __name__ == '__main__':
+   app.run(debug = True)
         
